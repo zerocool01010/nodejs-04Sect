1b810c4d0a53bdfdcd5851042141b7e019000533
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-03 13:17:51 -0300

    Agrego otro condicional para que no se vaya a la pausa si ya seleccione la opcion 0

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const main = async () => {
     opt = await mostrarMenu(); //lo que resuelva la promesa va a volver por el parametro del resolve() y lo guardo en la variable creada
     console.log('La opcion elegida es: ', opt)
 
-    await pausa() //vamos a evitar que se limpie la consola con el console.clear cada vez
+    if (opt !== '0') { //si ya seleccionamos 0 no entramos aca, asi salimos directamente
+        await pausa() //vamos a evitar que se limpie la consola con el console.clear cada vez
+    }    
 
   } while (opt !== "0");
 };
