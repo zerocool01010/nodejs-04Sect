1f80ca3746fd57e6f38aa7a4249071f5d325b58d
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-03 12:17:46 -0300

    Agrego una funcion en mensajes.js que permite hacer una especie de BufferReader como el de Java pero para javascript, ingresando algun valor por consola que puede ser leido por el programa

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -5,6 +5,24 @@ const mostrarMenu = () => {
     console.log('==================='.green)
     console.log('Selecciona una opcion'.red) //le doy color
     console.log('==================='.green)
+
+    console.log(`${'1.'.green} Crear tarea`)
+    console.log(`${'2.'.green} Listar tarea`)
+    console.log(`${'3.'.green} Listar tareas completadas`)
+    console.log(`${'4.'.green} Listar tareas pendientes`)
+    console.log(`${'5.'.green} Completar tarea(s)`)
+    console.log(`${'6.'.green} Borrar tarea`)
+    console.log(`${'0.'.green} Salir \n`)
+
+    const readline = require('readline').createInterface({ //creo la interface para entrar info desde la consola, como el BufferReader de Java
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    readline.question('Seleccione una opcion: ', (opt) => {
+        console.log({opt});
+        readline.close();
+    })
 }
 
 module.exports = {
